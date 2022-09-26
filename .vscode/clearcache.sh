24044e644d92ae0a014e28887e8f1be5b84383a2
AS400JPLPC <laroche.jeanpierre@gmail.com>
2022-09-26 20:12:39 +0200

    nouveau fichier : .vscode/ConvCss.sh 	nouveau fichier : .vscode/ConvGlade.sh 	nouveau fichier : .vscode/clearcache.sh 	nouveau fichier : .vscode/compilelib.sh 	modifié :         README.md

---
+++ .vscode/clearcache.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+projet_src=$1
+projet_lib=$2
+folder_src=$3
+
+
+folder_len=${#folder_src}
+
+lib_len=${#projet_lib}
+
+rlen=$(($lib_len-$folder_len))
+
+projet_bin=${projet_src%.*}
+
+lib_projet="$(cut -c 1-$rlen <<< $projet_lib)"
+
+folder_bin=$lib_projet"zig-out/bin/"$projet_bin
+
+folder_cache=$lib_projet"zig-cache"
+
+folder_cachetest=$lib_projet$folder_src"/zig-cache"
+
+
+
+#echo -en $folder_bin\\n
+#echo -en $lib_projet\\n
+#echo -en $projet_src\\n
+#echo -en $folder_src\\n
+#echo -en $projet_typ\\n
+#echo -en $projet_bin\\n
+#echo -en $folder_cachetest\\n
+
+
+if test -d $folder_cache ; then
+	rm -r $folder_cache
+fi
+if test -f $projet_bin ; then
+	rm -r $projet_bin
+fi
+
+if test -d $folder_cachetest ; then
+	rm -r $folder_cachetest
+fi
